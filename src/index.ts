c5151c69096d26314a30df7ad4ccbc014bbad7eb
hadi arbabi <hadimojak@gmail.com>
2025-08-13 16:27:13 +0330

    init

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "./User";
-import { Company } from "./Company";
-
-const user = new User();
-const company = new Company();
-
-console.log(user.getProperties);
-console.log(company.getAll);
