057366202e06e0a9e0d67169b5f63de38257ece1
hadi arbabi <hadimojak@gmail.com>
2025-08-18 17:27:50 +0330

    sorting algoritm

---
+++ src/sort/StringsCollection.ts
@@ -0,0 +1,17 @@
+export class StringsCollection {
+  constructor(public data: string) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+  compare(leftIndex: number, rightIndex: number): boolean {
+    return this.data[leftIndex].charCodeAt(0) > this.data[rightIndex].charCodeAt(0);
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const stringArr = this.data.split("");
+    [stringArr[leftIndex], stringArr[rightIndex]] = [stringArr[rightIndex], stringArr[leftIndex]];
+
+    this.data = stringArr.join("");
+  }
+}
--- src/sort/index.ts
+++ src/sort/index.ts
@@ -1,1 +1,15 @@
-console.log('herrrrrrrrrrrr');
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { StringsCollection } from './StringsCollection';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const stringsCollection = new StringsCollection('hjhgfrt7878676');
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection.data)
+
+
+const stringSorter = new Sorter(stringsCollection);
+stringSorter.sort();
+console.log(stringsCollection.data)
+
